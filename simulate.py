dc42595cc568ebd656c4620c823392e8e00fce6c
Nikitha Gollamudi <nikitha.gollamudi@gmail.com>
2022-04-26 01:30:11 -0500

    Add TD learning simulate algo

--- simulate.py
+++ simulate.py
@@ -3,7 +3,7 @@ import numpy as np
 from env import Env 
 from policy import Policy
 
-def Simulate(env: Env, policy: Policy, max_num_steps:int) -> int:
+def Simulate_MC(env: Env, policy: Policy, max_num_steps:int) -> int:
 	steps = 0
 	state = env.reset()
 	while True:
@@ -22,5 +22,40 @@ def Simulate(env: Env, policy: Policy, max_num_steps:int) -> int:
 			return -1	
 
 		state = new_state
+
+
+def Simulate_TD(env: Env,
+				 policy: Policy,
+				 num_episode: int,
+				 gamma: int,
+				 alpha: int,
+				 start_state:int) -> float:
+	V = np.zeros(env._nS)
+
+	for episode in range(0, num_episode):
+		state = env.reset()
+		steps = 0
+		while True:
+			action = policy.action(state)
+
+			new_state, cost, done, goal = env.step(action)
+
+			if done and goal:
+				V[state] = V[state] - alpha*V[state]
+				break
+
+			if done:
+				V[state] = V[state] + alpha*(1 - V[state])
+				break
+
+			V[state] = V[state] + alpha*(gamma*V[new_state] - V[state])
+
+
+			state = new_state
+
+	return V[start_state]
+
+
+
 
 
--- test_discrete.py
+++ test_discrete.py
@@ -1,7 +1,10 @@
 import numpy as np
 from env_discrete import DiscreteEnv
 from Q_learning_discrete import QLearning
-from simulate import Simulate
+from simulate import Simulate_MC
+
+from simulate import Simulate_TD
+
 import matplotlib.pylab as plt
 
 from metric import Metric
@@ -12,6 +15,8 @@ lambda2 = 0 #state cost
 
 
 primary_prob = 0.9
+
+start_state = 10
 
 state_matrix = np.array([[-1, -1, -1, -1, -1, -1, -1, -1],
                           [-1, 0, 2, 0, 0, 0, 0, -1],
@@ -35,13 +40,13 @@ num_episode = 100000
 epsilon = 0.1 #not using this, epsilon is set to 1/itr
 
 #max_num_steps = 100 #This is to check if there is a loop or not
-runs = 1
+runs = 10
 #max_simulated_runs = 100
 ######################################################################################
 
-# failure_prob_with_eta = {}
+failure_prob_with_eta = {}
 
-
+num_episode_simulated = 1000
 
 
 for run in range(0, runs):
@@ -57,29 +62,31 @@ for run in range(0, runs):
 	    initQ,
 	    epsilon,
 	    False)
+
+	eta = eta + 10
 	
-	print(Q_star[env.reset()])
-	pi_star.print_all()
-	plt.figure(1)
-	plt.plot(metric.get_v_star_start())
-	plt.ylabel('V star start')
-	plt.figure(2)
-	plt.plot(metric.get_a_star_start())
-	plt.ylabel('a star start')
-	plt.figure(3)
-	plt.plot(metric.get_q_star_start(1), label='W')
-	plt.plot(metric.get_q_star_start(3), label='E')
-	plt.legend(loc="upper right")
-	plt.ylabel('Q (W, E)')
+	# print(Q_star[env.reset()])
+	# pi_star.print_all()
+	# plt.figure(1)
+	# plt.plot(metric.get_v_star_start())
+	# plt.ylabel('V star start')
+	# plt.figure(2)
+	# plt.plot(metric.get_a_star_start())
+	# plt.ylabel('a star start')
+	# plt.figure(3)
+	# plt.plot(metric.get_q_star_start(1), label='W')
+	# plt.plot(metric.get_q_star_start(3), label='E')
+	# plt.legend(loc="upper right")
+	# plt.ylabel('Q (W, E)')
 # 	plt.figure(4)
 # 	plt.plot(num_steps)
 # 	plt.ylabel('num steps')
-	plt.show()
+	# plt.show()
 
 
 # 	failed_runs = 0
 # 	for simulate_run in range(0, max_simulated_runs):
-# 		result = Simulate(env, pi_star, max_num_steps)
+# 		result = Simulate_MC(env, pi_star, max_num_steps)
 
 # 		if (result == 0) :
 # 			failed_runs += 1
@@ -89,18 +96,20 @@ for run in range(0, runs):
 
 # 	failure_prob = failed_runs/max_simulated_runs
 
-# 	failure_prob_with_eta[eta] = failure_prob
+	failure_prob = Simulate_TD(env, pi_star, num_episode_simulated, gamma, alpha, start_state)
 
-# 	print("failure_prob", failure_prob, failed_runs)
+	failure_prob_with_eta[eta] = failure_prob
+
+	print("failure_prob", failure_prob)
 
 
 # #Plotting eta with failure probability
-# lists = sorted(failure_prob_with_eta.items())
+lists = sorted(failure_prob_with_eta.items())
 
-# x, y = zip(*lists)
+x, y = zip(*lists)
 
-# plt.plot(x, y)
-# plt.show()
+plt.plot(x, y)
+plt.show()
 
 
 
